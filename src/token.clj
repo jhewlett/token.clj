4bf529d7f4da59ca46d0574f2c026a001f4fa09d
Justin Hewlett <hewlett.jus@gmail.com>
2014-09-17 22:30:54 -0600

    re-order functions. return list of tokens instead of just true/false

--- src/token.clj
+++ src/token.clj
@@ -1,36 +1,39 @@
 (ns token)
-(def rules [#"[a-z]"
-            #"[a-zA-Z]+"
-            #" "
-            #"[0-9]+"])
 
-(defn process [rules text]
-  (if (empty? text)
-    true
-     (let [match (get-first-match rules text)]
-     (if (seq match)
-       (process rules (subs text (match 1)))
-       false
-       ))))
+(def rules [[#"[a-z]+" :LOWER-ID]
+            [#"[a-zA-Z]+" :UPPER-ID]
+            [#" " :SPACE]
+            [#"[0-9]+" :NUM]])
+
+(defn get-next-match [[regex token] text]
+  (let [matcher (re-matcher regex text)]
+    (if (.find matcher)
+      (let [start (.start matcher)
+            end (.end matcher)
+            length (- end start)]
+        {:start start :end end :length length :token token})
+      {}
+      )))
+
+(defn get-all-matches [rules text]
+  (reverse
+    (sort-by (fn [match] (:length match))
+     (filter (fn [match] (= 0 (:start match)))
+          (for [rule rules] (get-next-match rule text))))))
 
 (defn get-first-match [rules text]
   (first (get-all-matches rules text)))
 
-(defn get-all-matches [rules text]
-  (reverse
-    (sort-by (fn [range] (length range))
-     (filter (fn [range] (and (seq range) (zero? (range 0))))
-          (for [rule rules] (get-next-match rule text))))))
-
-(defn get-next-match [regex text]
-  (let [matcher (re-matcher regex text)]
-    (if (.find matcher)
-      [(.start matcher) (.end matcher)]
-      []
-      )))
-
-(defn length [[start end]]
-  (- end start))
-
-; todo: use map instead of range vector? with built-in length fn?
-; todo: return list of tokens (how to handle error? exception?)
+(defn process-helper [rules text tokens]
+  (if (empty? text)
+    tokens
+     (let [match (get-first-match rules text)]
+     (if (seq match)
+       (let [remaining-text (subs text (:end match))
+             all-tokens (conj tokens (:token match))]
+         (process-helper rules remaining-text all-tokens))
+       []
+       ))))
+  
+(defn process [rules text]
+  (process-helper rules text []))
