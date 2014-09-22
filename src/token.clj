c84c13a27653094355a1a15e6de7008cd2d3b577
Justin Hewlett <hewlett.jus@gmail.com>
2014-09-21 22:29:34 -0600

    do not add :IGNORE tokens to list

--- src/token.clj
+++ src/token.clj
@@ -2,8 +2,9 @@
 
 (def rules [[#"[a-z]+" :LOWER-ID]
             [#"[a-zA-Z]+" :UPPER-ID]
-            [#" " :SPACE]
-            [#"[0-9]+" :NUM]])
+            [#"\s+" :SPACE]
+            [#"[0-9]+" :NUM]
+            [#"&" :IGNORE]])
 
 (defn get-next-match [[regex token] text]
   (let [matcher (re-matcher regex text)]
@@ -22,18 +23,21 @@
         ordered-by-match-length (sort-by (fn [match] (:length match)) matches-at-start)]
     (last ordered-by-match-length)))
 
+(defn append-token-if-not-ignored [tokens this-token]
+  (if (= this-token :IGNORE)
+    tokens
+    (conj tokens this-token)))
+
 (defn process-helper [rules text tokens]
   (if (empty? text)
     tokens
      (let [match (get-best-match rules text)]
      (if (seq match)
        (let [remaining-text (subs text (:end match))
-             all-tokens (conj tokens (:token match))]
+             all-tokens (append-token-if-not-ignored tokens (:token match))]
          (process-helper rules remaining-text all-tokens))
        []
        ))))
   
 (defn process [rules text]
   (process-helper rules text []))
-
-;todo: take first rule if two things match with same length
